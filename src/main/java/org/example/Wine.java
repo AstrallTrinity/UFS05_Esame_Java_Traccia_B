4a4c9ba788d70f96fda605676e24eda1fe8e6fcc
AstrallTrinity <101412297+AstrallTrinity@users.noreply.github.com>
2022-05-23 14:26:46 +0200

    feat: insert java class Wine

---
+++ src/main/java/org/example/Wine.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Wine {
+    int id;
+    String name;
+    double price;
+    String type;
+    Wine(){}
+
+    public Wine(int id, String name, double price, String type) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.type = type;
+
+    }
+
+
+}
